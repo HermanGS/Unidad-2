0587d65dcdc8ee78fda7eeb32f16cc79b0e7e756
Herman Gabriel Soria <103910909+HermanGS@users.noreply.github.com>
2022-05-10 09:05:18 -0300

    Add files via upload

--- Ejercicio6/ViajeroFrecuente.py
+++ Ejercicio6/ViajeroFrecuente.py
@@ -1,2 +1,49 @@
-if __name__ == '__main__':
-    print("hello world")
+class ViajeroFrecuente:
+    __num_viajero= 0
+    __dni = ""
+    __nombre= ""
+    __apellido=""
+    __millas_acum= 0
+    def __init__(self, num, dni, nom, ap, mil=0):
+        if (type(num)==int)&(type(dni)==int)&(type(nom)==str)&(type(ap)==str)&(type(mil)==int):
+            self.__num_viajero=num
+            self.__dni=dni
+            self.__nombre=nom
+            self.__apellido=ap
+            self.__millas_acum=mil
+        else:
+            print("Error al cargar datos de viajero \n")
+    def __str__(self):
+        return("id viajero: {}\nDNI: {}\nNombre y apellido:{}, {}\nCon cantidad de millas: {}\n".format(self.__num_viajero, self.__dni, self.__nombre, self.__apellido, self.__millas_acum))
+    def CantidadTotalMillas(self):
+        return(self.__millas_acum)
+    def AcumularMillas(self, mill):
+        if (type(mill)==int):
+            self.__millas_acum=self.__millas_acum+mill
+            return (self.__millas_acum)
+        else:
+            print("Error al ingresar cantidad de millas a acumular \n")
+    def CanjearMillas(self, mill):
+        if(type(mill)==int):
+            if (self.__millas_acum>=mill):
+                self.__millas_acum=self.__millas_acum-mill
+                return(self.__millas_acum)
+            else:
+                print("Cantidad de millas no es suficiente\n")
+            return (self.__millas_acum)
+        else:
+            print("Error al ingresar cantidad de millas al canjear")
+    def getnumerodeviajero(self):
+        return (self.__num_viajero)
+    def __gt__ (self, otro):
+        if (type(otro)==type(self)):
+            if(self.__millas_acum > otro.__millas_acum):
+                return (True)
+            else:
+                return (False)
+        else:
+            return("Objeto no compatible")
+    def __add__(self, sumando):
+        return(self.AcumularMillas(sumando))
+    def __sub__(self, restando):
+        return(self.CanjearMillas(restando))
--- Ejercicio6/main.py
+++ Ejercicio6/main.py
@@ -1,2 +1,21 @@
-if __name__ == '__main__':
-    print("hello world")
+from ViajeroFrecuente import ViajeroFrecuente
+
+
+import ViajeroFrecuente
+
+if __name__=="__main__":
+    Viajero1=ViajeroFrecuente.ViajeroFrecuente(1, 4214124, 'Juan', 'Perez', 150)
+    Viajero2=ViajeroFrecuente.ViajeroFrecuente(2, 4213333, 'Jose', 'Alvarez', 300)
+    print ("Millas iniciales viajero 1: {}\n".format(Viajero1.CantidadTotalMillas()))
+    numero=250
+    print ("El viajero 1 acumulo {} \nmillas actuales: {}\n".format(numero, Viajero1+numero))
+    numero=50
+    print ("El viajero 1 canjeo {} \nmillas actuales: {}\n".format(numero, Viajero1-numero))
+    print("\nCOMPARACION\n")
+    for i in range(2):
+        if (Viajero1>Viajero2):
+            print ("El viajero con datos:\n\n{}\nTiene mas millas".format(str(Viajero1)))
+            numero=200
+            print ("El viajero 1 canjeo {} \nmillas actuales: {}\n".format(numero, Viajero1-numero))
+        else:
+            print ("El viajero con datos:\n{}\nTiene mas millas o ambos tienen la misma cantidad de millas".format(str(Viajero2)))
