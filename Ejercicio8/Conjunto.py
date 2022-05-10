2597c06f850c98179a4526824cb8ffb0f200a48d
Herman Gabriel Soria <103910909+HermanGS@users.noreply.github.com>
2022-05-10 08:53:27 -0300

    Add files via upload

--- Ejercicio8/Conjunto.py
+++ Ejercicio8/Conjunto.py
@@ -1,2 +1,67 @@
-if __name__ == '__main__':
-    print("hello world")
+class Conjunto:
+    def __init__(self):
+        self.__Numeros=[]
+    def Agregarelemento(self, numero):
+        if (type(numero)==int): 
+            self.__Numeros.append(numero)
+    def __str__(self):
+        return("{}".format(self.__Numeros))
+    def test1(self):
+        self.Agregarelemento(1)
+        self.Agregarelemento(2)
+        self.Agregarelemento(3)
+        self.Agregarelemento(4)
+        self.Agregarelemento(5)
+    def test2(self):
+        self.Agregarelemento(1)
+        self.Agregarelemento(3)
+        self.Agregarelemento(6)
+        self.Agregarelemento(9)
+        self.Agregarelemento(4)
+    def QuitarRepetidos(self):
+        for i in range(len(self.__Numeros)-1):
+            j=i+1
+            while (j<len(self.__Numeros)):
+                if self.__Numeros[i]==self.__Numeros[j]:
+                    self.__Numeros.pop(j)
+                j=j+1
+    def __add__(self, otro):
+        resultado=None
+        if type(otro)==type(self):
+            Union=Conjunto()
+            for i in self.__Numeros:
+                Union.Agregarelemento(i)
+            for j in otro.__Numeros:
+                Union.Agregarelemento(j)
+            Union.QuitarRepetidos()
+            resultado=Union
+        else:
+            print("Tipo de dato no valido")
+        return(resultado)
+    def __sub__(self, otro):
+        resultado=None
+        if type(otro)==type(self):
+            Interseccion=Conjunto()
+            for i in range(len(self.__Numeros)):
+                j=0
+                while ((j<len(otro.__Numeros)) and (self.__Numeros[i]!=otro.__Numeros[j])):
+                    j+=1
+                if j>=len(otro.__Numeros):
+                    Interseccion.Agregarelemento(self.__Numeros[i])
+            resultado=Interseccion
+        else:
+            print("Tipo de dato no valido")
+        return(resultado)
+    def __eq__(self, otro):
+        bandera=False
+        if (len(self.__Numeros)==len(otro.__Numeros)):
+            bandera=True
+            i=0
+            while (i<len(otro.__Numeros) and (bandera==True)):
+                j=0
+                while (j<len(otro.__Numeros) and (self.__Numeros[i]!=otro.__Numeros[j])):
+                    j+=1
+                if j>=len(otro.__Numeros):
+                    bandera=False
+                i+=1
+        return(bandera)  
--- Ejercicio8/main.py
+++ Ejercicio8/main.py
@@ -1,2 +1,20 @@
-if __name__ == '__main__':
-    print("hello world")
+import Conjunto
+
+if __name__=="__main__":
+    conjunto1=Conjunto.Conjunto()
+    conjunto1.test1()
+    conjunto2=Conjunto.Conjunto()
+    conjunto2.test2()
+    print("Conjunto 1:")
+    print(conjunto1)
+    print("Conjunto 2")
+    print(conjunto2)
+    print("Union de conjunto 1 y conjunto 2")
+    print(conjunto1+conjunto2)
+    print("Diferencia entre conjunto 1 y conjunto 2")
+    print(conjunto1-conjunto2)
+    print("Comparacion entre conjuntos")
+    if(conjunto1==conjunto2):
+        print("Son iguales")
+    else:
+        print("Son distintos")
